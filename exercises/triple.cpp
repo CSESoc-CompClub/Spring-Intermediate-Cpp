bffe9c58594852389f2b631301914cc6b14eed16
Ryan Berlee <z5358674@ad.unsw.edu.au>
2023-09-28 12:11:15 +1000

    structs exercise

---
+++ exercises/triple.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+struct triple {
+    int x;
+    int y;
+    int z;
+};
+
+// Anya wants to practice adding tripples together (of the format <x, y, z>),
+// and wants to test herself by writing a solver in code. Can you help her read
+// in two triples as input, and output the resulting sum?
+
+int main(void) {
+    struct triple first;
+    struct triple second;
+
+    // read in input and calculate sum here
+}
