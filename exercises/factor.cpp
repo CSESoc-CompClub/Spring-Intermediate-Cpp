d5fb27baf0b0860b2db1c40efaf21853986068d9
Ryan Berlee <z5358674@ad.unsw.edu.au>
2023-09-29 21:49:29 +1000

    added recap exercises

---
+++ exercises/factor.cpp
@@ -0,0 +1,21 @@
+// Anja needs even more help with her maths homework.
+// She needs help determining if a number is a multiple
+// of another number.
+
+// You can assume that the number will be an integer.
+// Expected output:
+
+// Enter the factor: 2
+// Enter the number: 10
+// The number 2 is a factor of 10
+
+// Enter the factor: 3
+// Enter the number: 10
+// The number 3 is not a factor of 10
+
+#include <iostream>
+
+int main(void) {
+    // Write your code here...
+    return 0;
+}
---
+++ exercises/timetable.cpp
@@ -0,0 +1,28 @@
+// Anya is still failing! Help her recite
+// her timetables all the way to 10.
+
+// Expected output:
+// Which timetables are we reciting? 4
+// 4 x 0 = 0
+// Yes!
+// 4 x 1 = 4
+// Yes!
+// 4 x 2 = 4
+// No! Try again!
+// 4 x 2 = 8
+// Yes!
+// ...
+// 4 x 10 = 40
+// Yes!
+// You did it!
+
+#include <iostream>
+
+int main(void) {
+    std::cout << "Which timetables are we reciting? ";
+
+    // Write your code here!
+
+    std::cout << "You did it! \n";
+    return 0;
+}
